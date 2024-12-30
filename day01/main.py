455ee55587c0f4011d1c97511ab695ff24137dc7
yure nobrega <yurenobrega221b@gmail.com>
2024-12-30 14:41:30 -0300

    Gerador de nome de banda

---
+++ day01/main.py
@@ -0,0 +1,4 @@
+print("Bem-vindo ao Gerador de Nomes de Bandas.")
+cidade = input("Qual é o nome da cidade onde você cresceu?\n")
+animal = input("Qual é o nome do seu animal de estimação?\n")
+print("O nome da sua banda pode ser " + cidade + " " + animal)
